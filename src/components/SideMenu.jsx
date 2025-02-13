c896802cd3ac5e7efcf801bfd16b0396819dde20
Sulaimon Omotosho <susudef2u@gmail.com>
2025-02-13 09:06:27 +0100

    Feat: Started PostList Page

---
+++ src/components/SideMenu.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SideMenu = () => {
+  return <div className=''>Side menu</div>
+}
+
+export default SideMenu
--- src/routes/PostListPage.jsx
+++ src/routes/PostListPage.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import SideMenu from '../components/SideMenu'
+import PostList from '../components/PostList'
 
 const PostListPage = () => {
-  return <div>PostListPage</div>
+  return (
+    <div>
+      <h1 className='mb-8 text-2xl'>Development Blog</h1>
+      <div className='flex gap-8'>
+        <div className=''>
+          <PostList />
+        </div>
+        <div className=''>
+          <SideMenu />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default PostListPage
