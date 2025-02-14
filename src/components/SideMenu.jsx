20e5d6f6e455680093aecad1e9e475a0cfe7c81e
Sulaimon Omotosho <susudef2u@gmail.com>
2025-02-14 13:25:19 +0100

    Feat: Posts page UI finished

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,7 +1,74 @@
 import React from 'react'
+import Search from '../components/Search'
+import { Link } from 'react-router'
 
 const SideMenu = () => {
-  return <div className=''>Side menu</div>
+  return (
+    <div className='px-4 h-max sticky top-8'>
+      <h1 className='mb-4 text-sm font-medium'>Search</h1>
+      <Search />
+      <h1 className='mb-4 mt-8 text-sm font-medium'>Filters</h1>
+      <div className='flex flex-col text-sm gap-2'>
+        <label htmlFor='' className='flex items-center gap-2 cursor-pointer'>
+          <input
+            type='radio'
+            name='sort'
+            value='latest'
+            className='appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800'
+          />
+          Latest
+        </label>
+        <label htmlFor='' className='flex items-center gap-2 cursor-pointer'>
+          <input
+            type='radio'
+            name='sort'
+            value='oldest'
+            className='appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800'
+          />
+          Oldest
+        </label>
+        <label htmlFor='' className='flex items-center gap-2 cursor-pointer'>
+          <input
+            type='radio'
+            name='sort'
+            value='trending'
+            className='appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800'
+          />
+          Trending
+        </label>
+        <label htmlFor='' className='flex items-center gap-2 cursor-pointer'>
+          <input
+            type='radio'
+            name='sort'
+            value='popular'
+            className='appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800'
+          />
+          Most Popular
+        </label>
+      </div>
+      <h1 className='mt-8 mb-4 text-sm font-medium'>Categories</h1>
+      <div className='flex flex-col gap-2 text-sm'>
+        <Link className='underline' to='/posts'>
+          All
+        </Link>
+        <Link className='underline' to='/posts?cat=web-design'>
+          Web Design
+        </Link>
+        <Link className='underline' to='/posts?cat=web-dev'>
+          Web Development
+        </Link>
+        <Link className='underline' to='/posts?cat=cloud'>
+          Cloud Engineering
+        </Link>
+        <Link className='underline' to='/posts?cat=seo'>
+          Search Engines
+        </Link>
+        <Link className='underline' to='/posts?cat=data'>
+          Data Management
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default SideMenu
--- src/routes/PostListPage.jsx
+++ src/routes/PostListPage.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SideMenu from '../components/SideMenu'
 import PostList from '../components/PostList'
 
 const PostListPage = () => {
+  const [open, setOpen] = useState(false)
+
   return (
     <div>
       <h1 className='mb-8 text-2xl'>Development Blog</h1>
-      <div className='flex gap-8'>
+      <button
+        onClick={() => setOpen(!open)}
+        className='bg-blue-800 text-sm text-white px-4 py-2 rounded-2xl mb-4 md:hidden'
+      >
+        {open ? 'Close' : 'Filter or Search'}
+      </button>
+      <div className='flex flex-col-reverse gap-8 md:flex-row'>
         <div className=''>
           <PostList />
         </div>
-        <div className=''>
+        <div className={`${open ? 'block' : 'hidden'} md:block `}>
           <SideMenu />
         </div>
       </div>
